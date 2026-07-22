4ba0ead871b2c31297b667c94fa817009d23be0e
Archit Satish Joshi <joshi.archit.2904@gmail.com>
2026-07-22 18:38:18 -0500

    feat: add tauri-plugin-dialog and update permissions in default.json

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,19 +1,13 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 mod scanner;
-
-use chrono::prelude::*;
 use scanner::scan_repos;
-
-#[tauri::command]
-fn get_date_time() -> DateTime<Utc> {
-    Utc::now()
-}
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![get_date_time, scan_repos])
+        .plugin(tauri_plugin_dialog::init())
+        .invoke_handler(tauri::generate_handler![scan_repos])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }

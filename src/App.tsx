976ce6e895fd536e8757475aaa7338fca8eb72e0
Archit Satish Joshi <joshi.archit.2904@gmail.com>
2026-07-23 20:07:58 -0500

    feat: update Sidebar component to be collapsible and style updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { useState } from "react";
 import "./App.css";
-import type { RepoEntry } from "./types/repo.types";
 import Sidebar from "./components/Sidebar";
+import type { RepoEntry } from "./types/repo.types";
 
 function App() {
+  const [activeRepo, setActiveRepo] = useState<RepoEntry>();
+
   return (
-    <main className="bg-black flex h-screen w-screen p-2 font-mono">
-      <Sidebar />
+    <main className="relative flex h-screen w-screen gap-4 bg-black p-2 font-mono">
+      <div className="w-sidebar-collapsed shrink-0" />{" "}
+      {/* reserves collapsed-width space */}
+      <div className="absolute inset-y-2 left-2 z-10">
+        <Sidebar setActiveRepo={setActiveRepo} />
+      </div>
+      <div className="h-full w-full rounded-xl bg-gray-950"></div>
     </main>
   );
 }
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,15 +1,22 @@
-import React, { useState } from "react";
-import { RepoEntry } from "../types/repo.types";
 import { invoke } from "@tauri-apps/api/core";
 import { open } from "@tauri-apps/plugin-dialog";
+import { FolderGit2, PanelRight, Sprout } from "lucide-react";
+import { useState } from "react";
+import { RepoEntry } from "../types/repo.types";
+
+type SidebarProps = {
+  setActiveRepo: (repo: RepoEntry) => void;
+};
 
 /**
  * A sidebar component that displays a list of git repositories, a button to scan a root directory and selecting a repository to view its details.
  * @returns The rendered sidebar component.
  */
-const Sidebar = () => {
-  let rootPath: string = "";
+const Sidebar = ({ setActiveRepo }: SidebarProps) => {
+  const [collapsed, setCollapsed] = useState(true);
   const [repoList, setRepoList] = useState<RepoEntry[]>([]);
+
+  let rootPath: string = "";
 
   async function getRepoList() {
     const result = await invoke<RepoEntry[]>("scan_repos", {
@@ -30,46 +37,90 @@ const Sidebar = () => {
     await getRepoList();
   }
 
+  function handleSetActiveRepo(repo: RepoEntry) {
+    setActiveRepo(repo);
+  }
+
   return (
     <aside
       id="sidebar"
-      className="box-border flex h-full w-64 flex-col gap-3 rounded-xl bg-gray-900 p-4 text-white"
+      className={`${collapsed ? "w-sidebar-collapsed" : "w-64 lg:w-[20vw]"} z-10 box-border flex h-full flex-col gap-3 rounded-xl bg-gray-900 p-4 text-white transition-all duration-300`}
     >
-      <section id="title">
-        <span className="flex w-full items-center justify-start text-xl">
-          Git Grove
-        </span>
+      <section
+        id="title"
+        className={`${collapsed ? "justify-center" : "justify-between"} flex w-full items-center gap-1 border-b border-gray-500 pb-2 text-xl`}
+      >
+        <div className="flex items-center justify-start gap-1">
+          {/* TODO: Create a product icon */}
+          <Sprout size={18} />
+          <span
+            className={`overflow-hidden whitespace-nowrap transition-all duration-300 ${collapsed ? "w-0 opacity-0" : "ml-1 w-auto opacity-100"}`}
+          >
+            GitGrove
+          </span>
+        </div>
+        <PanelRight
+          onClick={() => setCollapsed(true)}
+          className={collapsed ? "hidden" : ""}
+        />
       </section>
+
       <section
         id="sidebar-action-buttons"
-        className="flex w-full items-center justify-start gap-6"
+        className={`flex w-full flex-col items-center justify-center gap-4 ${collapsed ? "items-center" : "items-start"}`}
       >
+        <PanelRight
+          onClick={() => setCollapsed(false)}
+          className={collapsed ? "" : "hidden"}
+        />
         <button
-          className="flex items-center justify-center rounded-lg border border-transparent bg-green-600 px-4 py-2 text-sm transition-transform duration-100 hover:cursor-pointer hover:bg-green-900 active:scale-95 active:bg-green-950"
           onClick={getRootDirectoryPath}
+          title="Scan for Repos"
+          className={`flex items-center justify-center overflow-hidden rounded-lg border border-transparent bg-green-600 p-2 text-sm transition-all duration-300 hover:cursor-pointer hover:bg-green-700 active:scale-95 active:bg-green-800 ${collapsed ? "" : "w-full"}`}
         >
-          Scan for Repos
+          <FolderGit2 className="shrink-0" />
+          <span
+            className={`overflow-hidden whitespace-nowrap transition-all duration-300 ${collapsed ? "w-0 opacity-0" : "ml-2 w-auto opacity-100"}`}
+          >
+            Scan for Repos
+          </span>
         </button>
-        {/* // TODO: Enhancement
-        <button>Add Repo</button> */}
+        {/* TODO: Enhancement, needs adjustments in the Rust Command */}
+        {/* <button
+          onClick={getRootDirectoryPath}
+          title="Scan for Repos"
+          className={`flex items-center justify-center overflow-hidden rounded-lg border border-gray-500 bg-transparent p-2 text-sm transition-all duration-300 hover:cursor-pointer hover:bg-gray-800 ${collapsed ? "" : "w-full"}`}
+        >
+          <FolderCode className="shrink-0" />
+          <span
+            className={`overflow-hidden whitespace-nowrap transition-all duration-300 ${collapsed ? "w-0 opacity-0" : "ml-2 w-auto opacity-100"}`}
+          >
+            Add a Repo
+          </span>
+        </button> */}
       </section>
-      <section
-        id="sidebar-repo-list"
-        className="min-h-0 flex-1 scrollbar-thin scrollbar-thumb-blue-300 overflow-y-auto"
-      >
-        <ul className="list-none space-y-2">
-          {repoList.map((repo) => {
-            return (
-              <li
-                key={repo.name}
-                className="transition-color hover: cursor-pointer rounded-lg border border-transparent px-2 text-sm duration-300 hover:bg-blue-600"
-              >
-                {repo.name}
-              </li>
-            );
-          })}
-        </ul>
-      </section>
+
+      {!collapsed && (
+        <section
+          id="sidebar-repo-list"
+          className="min-h-0 flex-1 scrollbar-thin scrollbar-thumb-blue-300 overflow-y-auto transition-all duration-300"
+        >
+          <ul className="list-none space-y-2">
+            {repoList.map((repo) => {
+              return (
+                <li
+                  key={repo.name}
+                  className="transition-color hover: cursor-pointer rounded-lg border border-transparent px-2 text-sm duration-300 hover:bg-blue-600"
+
+                  onClick={() => handleSetActiveRepo(repo)}
+                >
+                  {repo.name}
+                </li>
+              );
+            })}
+          </ul>
+        </section>
+      )}
     </aside>
   );
 };

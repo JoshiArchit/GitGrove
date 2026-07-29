ecee02bb5cac153a2ec88063cbe1cc1dddd9f18b
Archit Satish Joshi <joshi.archit.2904@gmail.com>
2026-07-28 19:15:03 -0500

    feat: add support for tauri-plugin-store in the application

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -10,6 +10,7 @@ use summary::get_repo_summary;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
         .invoke_handler(tauri::generate_handler![

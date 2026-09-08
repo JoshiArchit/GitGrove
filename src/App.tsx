5bf1a6f7623d3cfe97b39e00ab82f51c532d7e0a
Archit Satish Joshi <joshi.archit.2904@gmail.com>
2026-09-08 18:38:42 -0500

    feat: add Taskboard component with BoardColumn for task management

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import ContributionGraph from "./components/ContributionGraph";
 import RepoSummaryData from "./components/RepoSummary/RepoSummary";
 import Sidebar from "./components/Sidebar";
 import Taskboard from "./components/Taskboard/Taskboard";
+import Taskboard from "./components/Taskboard/Taskboard";
 import WelcomeScreen from "./components/WelcomeScreen";
 import { usePersistedRepoList } from "./hooks/usePersistedRepoList";
 import { useSelectedRepoStore } from "./stores/selectedRepoStore";

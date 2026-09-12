bfc0e9469f352bff1806c1e77278772712a2af64
Archit Satish Joshi <joshi.archit.2904@gmail.com>
2026-09-12 12:59:44 -0500

    feat: enhance TaskForm to handle work item creation and improve form structure

--- src/components/Taskboard/TaskForm.tsx
+++ src/components/Taskboard/TaskForm.tsx
@@ -1,20 +1,46 @@
 import React from "react";
+import { useTaskBoardStore } from "../../store/taskBoardStore";
 import { useSelectedRepoStore } from "../../stores/selectedRepoStore";
-import { WorkItemtype } from "../../types/board.types";
+import { Status, WorkItem, WorkItemtype } from "../../types/board.types";
 
 type TaskFormProps = {
   onClose: () => void;
 };
 
+type TaskFormValues = {
+  title: string;
+  description: string;
+  branch: string;
+  type: WorkItemtype;
+};
+
 const TaskForm = ({ onClose }: TaskFormProps) => {
   const branches = useSelectedRepoStore((s) => s.summary?.branches);
+  const repoPath = useSelectedRepoStore((s) => s.repo!.path);
+  const addItem = useTaskBoardStore((s) => s.addItem);
 
   function handleSubmit(e: React.SubmitEvent<HTMLFormElement>) {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
-    console.log(Object.fromEntries(formData));
+    //  Convert FormData to an object for easier handling
+    const data = Object.fromEntries(formData) as unknown as TaskFormValues;
+
+    // Create a WorkItem
+    const workItem: WorkItem = {
+      id: crypto.randomUUID(),
+      type: data.type,
+      title: data.title,
+      description: data.description,
+      branch: data.branch,
+      status: Status.Backlog,
+      tasks: [],
+    };
+
+    // Add work item to store
+    addItem(repoPath, workItem);
     //  Reset the form after submission
     e.currentTarget.reset();
+
     onClose();
   }
 
@@ -55,10 +81,10 @@ const TaskForm = ({ onClose }: TaskFormProps) => {
         </div>
 
         <div className="flex w-1/2 flex-col items-start justify-center gap-2">
-          <label htmlFor="workitem-type">Select work item type</label>
+          <label htmlFor="type">Select work item type</label>
           <select
-            id="workitem-type"
-            name="workitem-type"
+            id="type"
+            name="type"
             className="w-full rounded-2xl border-2 border-solid border-gray-500 px-4 py-2 focus-visible:outline-none"
           >
             {Object.values(WorkItemtype).map((e) => {

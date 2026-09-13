512aa4737be2e91adc0ef9ffcc3067c7af82b2fb
Archit Satish Joshi <joshi.archit.2904@gmail.com>
2026-09-12 19:21:46 -0500

    feat: implement WorkItemCard component and update BoardColumn to display work items

--- src/components/WorkItemBoard/BoardColumn.tsx
+++ src/components/WorkItemBoard/BoardColumn.tsx
@@ -1,4 +1,5 @@
 import { Status, WorkItem } from "../../types/board.types";
+import WorkItemCard from "./WorkItemCard";
 
 type BoardColumnProps = {
   columnStatus: Status;
@@ -7,8 +8,18 @@ type BoardColumnProps = {
 
 const BoardColumn = ({ columnStatus, workItems }: BoardColumnProps) => {
   return (
-    <section className="flex w-1/3 flex-col items-center justify-between rounded-lg bg-black px-2 py-4">
+    <section className="flex w-1/3 flex-col items-center justify-start gap-4 rounded-lg bg-black px-2 py-4">
       <span className="text-sm">{columnStatus}</span>
+
+      <section className="flex w-full flex-col gap-2">
+        {workItems.map((item) => {
+          return (
+            <div key={item.id}>
+              <WorkItemCard item={item} />
+            </div>
+          );
+        })}
+      </section>
     </section>
   );
 };
--- src/components/WorkItemBoard/WorkItemCard.tsx
+++ src/components/WorkItemBoard/WorkItemCard.tsx
@@ -4,7 +4,11 @@ type WorkItemCardProps = {
   item: WorkItem;
 };
 const WorkItemCard = ({ item }: WorkItemCardProps) => {
-  return <div>Task</div>;
+  return (
+    <div className="w-full rounded-lg border-2 border-gray-600 px-4 py-2 hover:cursor-pointer hover:bg-gray-400">
+      <span>{item.title}</span>
+    </div>
+  );
 };
 
 export default WorkItemCard;

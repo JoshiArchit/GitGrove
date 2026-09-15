36604356ef0f597a08f6b00bcf1a0172200f6124
Archit Satish Joshi <joshi.archit.2904@gmail.com>
2026-09-14 19:47:47 -0500

    feat: update Task type to use TaskStatus enum for improved status tracking and update border color resolution for work item

--- src/components/WorkItems/WorkItemCard.tsx
+++ src/components/WorkItems/WorkItemCard.tsx
@@ -39,12 +39,9 @@ const WorkItemCard = ({ item }: WorkItemCardProps) => {
   return (
     <>
       <div
-        className="flex w-full items-center gap-2 rounded-lg border-2 border-gray-600 px-4 py-2 hover:cursor-pointer hover:bg-gray-400"
+        className={`flex w-full items-center gap-2 rounded-lg border-2 border-l-8 border-gray-600 px-4 py-2 hover:cursor-pointer hover:bg-gray-400 ${itemColor.borderLeft}`}
         onClick={openDialog}
       >
-        <span
-          className={`inline-block h-3 w-3 rounded-full ${itemColor.bullet}`}
-        ></span>
         <span>{item.title}</span>
       </div>
 
